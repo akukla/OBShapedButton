b7957c77251daadb9f1b893ff13c7aeb66e238c1
Alexander Kukla <alexander.kukla@gmail.com>
2023-01-20 17:12:19 +0200

    Carthage support added

---
+++ ob-shaped-button/ob_shaped_button.h
@@ -0,0 +1,19 @@
+//
+//  ob_shaped_button.h
+//  ob-shaped-button
+//
+//  Created by Alexander Kukla on 20.01.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ob_shaped_button.
+FOUNDATION_EXPORT double ob_shaped_buttonVersionNumber;
+
+//! Project version string for ob_shaped_button.
+FOUNDATION_EXPORT const unsigned char ob_shaped_buttonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ob_shaped_button/PublicHeader.h>
+
+#import <ob_shaped_button/UIImage+ColorAtPixel.h>
+#import <ob_shaped_button/OBShapedButton.h>
